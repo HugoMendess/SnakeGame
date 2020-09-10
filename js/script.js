a074079e82295764052df50e77ed0b4d746bb8ea
Hugo Mendes <hugo-vmendes@hotmail.com>
2020-09-10 20:46:56 -0300

    Adicionando a comida da cobrinha

--- js/script.js
+++ js/script.js
@@ -8,6 +8,11 @@ snake[0] = {
 }
 
 let direction = "right";
+// Definindo a posição random aleatoria da comida
+let food = {
+	x: Math.floor(Math.random() * 15 + 1) * box,
+	y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 
 function criarBG(){
@@ -20,6 +25,11 @@ function criarCobrinha(){
 		context.fillStyle = "green";
 		context.fillRect(snake[i].x, snake[i].y, box, box);
 	}
+}
+
+function drawFood(){
+	context.fillStyle = "red";
+	context.fillRect(food.x, food.y, box, box);
 }
 
 // Vai pegar o evento de clique "keydown" e chamar a funcao update
@@ -63,6 +73,7 @@ function iniciarJogo(){
 
 	criarBG();
 	criarCobrinha();
+	drawFood();
 
 	let snakeX = snake[0].x;
 	let snakeY = snake[0].y;
