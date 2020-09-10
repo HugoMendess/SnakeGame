7a85830c581803738c30d4e2e8839cd8fcd7d304
Hugo Mendes <hugo-vmendes@hotmail.com>
2020-09-10 20:24:44 -0300

    Adicionando movimentação a cobrinha

--- js/script.js
+++ js/script.js
@@ -22,6 +22,27 @@ function criarCobrinha(){
 	}
 }
 
+// Vai pegar o evento de clique "keydown" e chamar a funcao update
+document.addEventListener('keydown', update);
+
+function update(){
+	if(event.keyCode == 37 && direction != "right"){
+		direction = "left";
+	}
+
+	if(event.keyCode == 38 && direction != "down"){
+		direction = "up";
+	}
+
+	if(event.keyCode == 39 && direction != "left"){
+		direction = "right";
+	}
+
+	if(event.keyCode == 40 && direction != "up"){
+		direction = "down";
+	}
+}
+
 function iniciarJogo(){
 	criarBG();
 	criarCobrinha();
