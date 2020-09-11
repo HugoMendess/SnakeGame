470ca5cccf301a3728c94afc11c5a580424236b2
Hugo Mendes <hugo-vmendes@hotmail.com>
2020-09-10 22:11:31 -0300

    Alterando design

--- js/script.js
+++ js/script.js
@@ -18,19 +18,19 @@ let food = {
 
 
 function criarBG(){
-	context.fillStyle = "lightgreen";
+	context.fillStyle = "#8a2be2";
 	context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha(){
 	for(i=0; i<snake.length;i++){
-		context.fillStyle = "green";
+		context.fillStyle = "#00ced1";
 		context.fillRect(snake[i].x, snake[i].y, box, box);
 	}
 }
 
 function drawFood(){
-	context.fillStyle = "red";
+	context.fillStyle = "#00ff00";
 	context.fillRect(food.x, food.y, box, box);
 }
 
